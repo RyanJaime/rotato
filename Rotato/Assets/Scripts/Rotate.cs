488c505318a2d3aadbb631de7feb0d892179a6a6
RyanJaime <jaimeryan139@yahoo.com>
2018-01-15 21:53:57 -0800

    working animated rotation; not rotating in a way that feels good

---
+++ Rotato/Assets/Scripts/Rotate.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotate : MonoBehaviour {
+
+    private int rotateUpDown = 0;
+    private int rotateLR = 0;
+
+    private float oldAngleHorizontal;
+    private float oldAngleVertical;
+
+    private Quaternion startingRotation;
+    public float speed = 15;
+    private int timesHitHorizontal = 0;
+    private int timesHitVertical = 0;
+
+    // Use this for initialization
+    void Start () {
+        startingRotation = this.transform.rotation;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            timesHitHorizontal--;
+        }
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            timesHitHorizontal++;
+        }
+
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            timesHitVertical++;
+        }
+        if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            timesHitVertical--;
+        }
+
+        this.transform.rotation = Quaternion.Lerp(this.transform.rotation, Quaternion.Euler(timesHitVertical * 90, timesHitHorizontal * 90, 0), Time.deltaTime * speed);
+    }
+}
