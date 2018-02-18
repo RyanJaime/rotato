579d9e09357686d72e0e7f1420c85edb2d47def5
RyanJaime <jaimeryan139@yahoo.com>
2018-02-18 12:07:47 -0800

    more level done... lunchtime

--- RhythmGame/Assets/noteInformation.cs
+++ RhythmGame/Assets/noteInformation.cs
@@ -22,7 +22,7 @@ public class noteInformation : MonoBehaviour {
         else if (face.tag == "destroyMissedNotes")// && hasGoneThruCorrectFace == false)
         {
             print(face.tag);
-            StartCoroutine(destroyAfterSeconds(1));
+            //StartCoroutine(destroyAfterSeconds(1));
             //play particle. change color.
             //this.gameObject.GetComponent<Renderer>().enabled = false;
             //Destroy(gameObject);
